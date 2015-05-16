95ae6d65f0903d6013a86d7905a36e71c0c73bee
Miró Sorja <miro.sorja@digital-identity.com>
2015-05-16 13:55:39 +0300

    Getting data works, components work

--- src/js/components/Todo.js
+++ src/js/components/Todo.js
@@ -2,12 +2,23 @@
 'use strict';
 
 var React = require('react');
+window.React = React;
 
 var Todo = React.createClass({
-
+getDefaultProps: function() {
+  return {
+    data: {
+      title:"No todos!",
+      description:"Add a new todo"},
+    i: ""
+  };
+},
   render: function() {
     return (
-      <h1> I'm todo </h1>
+      <li>
+        <h1>{this.props.i + this.props.data.title}</h1>
+        <p>{this.props.data.description}</p>
+      </li>
     );
   }
 
--- src/js/index.js
+++ src/js/index.js
@@ -2,16 +2,41 @@
 'use strict';
 var React = require('react');
 var Todo = require('./components/Todo');
+var $ = require('jquery');
+var request = require('superagent');
 
 var App = React.createClass({
 
-  componentDidMount: function() {
-    console.log("ok");
+  getInitialState: function() {
+    return {
+      todos: []
+    };
+  },
+
+  componentWillMount: function() {
+    var self = this;
+    request.get('/todos')
+    .end(function(err, res){
+      console.log(res)
+      self.setState({
+        todos: res.body
+      });
+    });
   },
 
   render: function() {
+    var todos = this.state.todos
+    .map(function(val, i){
+      return <Todo key={i} i={i} data={val} />
+    });
+
     return (
-      <h1>hello</h1>
+      <div>
+        <h1>To Do -list</h1>
+        <ul>
+          {todos}
+        </ul>
+      </div>
     );
   }
 
